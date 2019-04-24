67942061ac4afb26068a3cfa6f7dcca1761b7f1d
Heidi Yu <15190296383@163.com>
2019-04-24 10:01:05 +0800

    add header

---
+++ src/Pages/home/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe646;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont ">&#xe645;</span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">
+      城市
+      <div class="dropdown-icon">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+.header
+  display:flex;
+  height:.86rem;
+  background:$bgColor;
+  color:#fff;
+  line-height:.86rem;
+  .header-left
+    width:.64rem;
+    float:left;
+    .back-icon
+      font-size:.4rem
+      text-align:center
+  .header-input
+    flex:1
+    background: #fff;
+    border: 1px;
+    margin: .12rem .2rem;
+    height: 0.62rem;
+    border-radius: 0.1rem;
+    color: #c0c4cb;
+    padding-left: .1rem;
+    line-height:.7rem;
+  .header-right
+    width:1.24rem;
+    float:right;
+    .dropdown-icon
+      width:0
+      height:0
+      border-top: .15rem solid #fff;
+      border-left: .15rem solid transparent;
+      border-right: .15rem solid transparent;
+      position: relative;
+      bottom: .5rem;
+      left: .7rem;
+</style>
